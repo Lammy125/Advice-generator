b5cf200a5a6217905540bb0d2d6b0da742dfd089
Adeyemo Peter <45455927+Lammy125@users.noreply.github.com>
2022-11-23 09:22:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let advisedId = document.querySelector("#advisedId");
 let adviseText = document.querySelector("#adviseText");
 let generateButton = document.querySelector("#generateButton");
-let baseUrl = "https://api.adviceslip.com/advice%22";
+let baseUrl = "https://api.adviceslip.com/advice";
 
 const generateAdvice = async () => {
     try {
         const response = await fetch (baseUrl);
-        console.log(response);
+        let data = await response.json();
+        console.log(data);
+        advisedId.innerText = `#${data.slip.id}`;
+        adviseText.innerText = `"${data.slip.advice}"`;
     }catch (error) {
         console.log(error);
     }
