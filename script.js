6fb41f621d956176b2609dfde56ad44f507e38f8
Adeyemo Peter <45455927+Lammy125@users.noreply.github.com>
2024-02-23 15:30:00 +0100

    added hover effect text

--- script.js
+++ script.js
@@ -4,14 +4,23 @@ let generateButton = document.querySelector("#generateButton");
 let baseUrl = "https://api.adviceslip.com/advice";
 
 const generateAdvice = async () => {
-    try {
-        const response = await fetch (baseUrl);
-        let data = await response.json();
-        console.log(data);
-        advisedId.innerText = `#${data.slip.id}`;
-        adviseText.innerText = `"${data.slip.advice}"`;
-    }catch (error) {
-        console.log(error);
-    }
+  try {
+    const response = await fetch(baseUrl);
+    let data = await response.json();
+    console.log(data);
+    advisedId.innerText = `#${data.slip.id}`;
+    adviseText.innerText = `"${data.slip.advice}"`;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+function showTooltip() {
+  var tooltip = document.getElementById("tooltip");
+  tooltip.style.display = "block";
 }
 
+function hideTooltip() {
+  var tooltip = document.getElementById("tooltip");
+  tooltip.style.display = "none";
+}
